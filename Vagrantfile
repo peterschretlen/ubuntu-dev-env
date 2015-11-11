75d6488f8cb1e6a7340eac99d57f061d85aebd53
pschretlen <peter.schretlen@gmail.com>
2015-11-10 21:50:10 -0500

    initial commit

---
+++ Vagrantfile
@@ -0,0 +1,27 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure(2) do |config|
+
+  config.vm.box = "box-cutter/ubuntu1404-desktop"
+  config.vm.network "forwarded_port", guest: 8000, host: 8080
+  config.vm.network "private_network", ip: "192.168.56.120"
+  config.vm.hostname = "ubuntu-dev-env"
+
+  config.vm.provider "virtualbox" do |vb|
+    # Display the VirtualBox GUI when booting the machine
+    vb.gui = true
+    vb.name = "ubuntu-dev-env"
+  
+    # Customize the amount of memory on the VM:
+    vb.memory = "4096"
+    vb.cpus = 2
+  end
+
+  # Copies your local gitconfig to the VM so you don't have to set it up again
+  config.vm.provision "file", source: "~/.gitconfig", destination: ".gitconfig"
+
+  # Installs all packages and tools 
+  config.vm.provision :shell, path: "provision.sh"
+
+end
---
+++ provision.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+#######################################
+# Set up the development environement
+#######################################
+
+PY_CHARM_VERSION=4.5.4
+
+PROVISIONED_ON=/etc/vm_provision_on_timestamp
+if [ -f "$PROVISIONED_ON" ]
+then
+  echo "VM was already provisioned at: $(cat $PROVISIONED_ON)"
+  echo "To run system updates manually login via 'vagrant ssh' and run 'apt-get update && apt-get upgrade'"
+  echo ""
+  exit
+fi
+
+#######################################
+# Add additional repositories
+
+# see: http://www.ubuntuupdates.org/ppa/sublime_text_3
+add-apt-repository ppa:webupd8team/sublime-text-3 
+
+# for installing Chrome
+wget -nv -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+
+apt-get update
+
+#######################################
+# Install tools
+
+# Setup tools
+apt-get install -y git
+apt-get install -y sublime-text-installer
+apt-get install -y default-jdk  #needed for pycharm
+apt-get install -y meld #visual diff/merge tool
+apt-get install -y jq #json query command line tool
+apt-get install -y vim
+apt-get install -y libldap2-dev libsasl2-dev #these are needed to compile python-ldap for atom
+
+# Setup python
+apt-get install -y python python-pip
+
+# Add docker
+curl -sSL https://get.docker.com/ | sh
+
+# allow using docker as a non-root user
+usermod -aG docker vagrant
+
+# Add PEP8, Virtualenv
+pip install pep8
+pip install virtualenv
+
+# Setup Sublime Package Managers
+chown -R vagrant:vagrant /home/vagrant/.config/
+SUBLIME_DIR="/home/vagrant/.config/sublime-text-3/"
+SUBLIME_PACKAGE_DIR=$SUBLIME_DIR"Packages/"
+SUBLIME_INSTALL_PACKAGE_DIR=$SUBLIME_DIR"Installed Packages/"
+
+mkdir -p "$SUBLIME_INSTALL_PACKAGE_DIR"
+wget -nv -P "$SUBLIME_INSTALL_PACKAGE_DIR" https://packagecontrol.io/Package%20Control.sublime-package
+
+# Add Sublime packages
+mkdir -p "$SUBLIME_PACKAGE_DIR"
+git clone https://github.com/SublimeText-Markdown/MarkdownEditing.git "$SUBLIME_PACKAGE_DIR"MarkdownEditing
+git clone https://github.com/colinta/SublimeFileDiffs.git "$SUBLIME_PACKAGE_DIR"SublimeFileDiffs
+git clone https://github.com/titoBouzout/SideBarEnhancements.git "$SUBLIME_PACKAGE_DIR"SideBarEnhancements
+git clone https://github.com/SublimeLinter/SublimeLinter3.git "$SUBLIME_PACKAGE_DIR"SublimeLinter
+git clone https://github.com/wbond/sublime_terminal.git "$SUBLIME_PACKAGE_DIR"sublime_terminal
+git clone https://github.com/jisaacks/GitGutter.git "$SUBLIME_PACKAGE_DIR"GitGutter
+git clone https://github.com/SublimeLinter/SublimeLinter-pep8.git "$SUBLIME_PACKAGE_DIR"SublimeLinter-pep8
+
+chown -R vagrant:vagrant "$SUBLIME_INSTALL_PACKAGE_DIR"
+chown -R vagrant:vagrant "$SUBLIME_PACKAGE_DIR"
+chown -R vagrant:vagrant "$SUBLIME_DIR"
+
+# Install PyCharm
+# Don't download if the package already exists locally on the host
+if [ ! -f /vagrant/pycharm-professional-$PY_CHARM_VERSION.tar.gz ]; then
+    echo "PyCharm not found. Downloading..."
+    wget -nv -P /vagrant/ http://download-cf.jetbrains.com/python/pycharm-professional-$PY_CHARM_VERSION.tar.gz
+fi
+
+tar -xvf /vagrant/pycharm-professional-$PY_CHARM_VERSION.tar.gz
+chown -R vagrant:vagrant pycharm-$PY_CHARM_VERSION/
+
+# Install Chrome
+apt-get install -y google-chrome-stable
+
+# Setup meld as the default merge and diff tool
+su - vagrant -c "git config --global merge.tool meld"
+su - vagrant -c "git config --global diff.tool meld"
+
+# Cache git credentials for period of 10 hours
+su - vagrant -c "git config --global credential.helper 'cache --timeout=36000'"
+
+# Tag the provision time:
+date > "$PROVISIONED_ON"
+
+echo "Successfully created Development VM"
