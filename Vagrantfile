23d69033b1716eb900d32e2ef1217570017e7773
pschretlen <peter.schretlen@gmail.com>
2016-08-26 15:13:24 -0400

    removing python, jdk, and chrome install

--- Vagrantfile
+++ Vagrantfile
@@ -5,7 +5,6 @@ Vagrant.configure(2) do |config|
 
   config.vm.box = "box-cutter/ubuntu1404-desktop"
   config.vm.network "forwarded_port", guest: 8000, host: 8080
-  #config.vm.network "private_network", ip: "192.168.56.120"
   config.vm.hostname = "ubuntu-dev-env"
 
   config.vm.provider "virtualbox" do |vb|
--- provision.sh
+++ provision.sh
@@ -4,8 +4,6 @@
 # Set up the development environement
 #######################################
 
-WEBSTORM_VERSION=11.0.3
-
 PROVISIONED_ON=/etc/vm_provision_on_timestamp
 if [ -f "$PROVISIONED_ON" ]
 then
@@ -21,10 +19,6 @@ fi
 # see: http://www.ubuntuupdates.org/ppa/sublime_text_3
 add-apt-repository ppa:webupd8team/sublime-text-3 
 
-# for installing Chrome
-wget -nv -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-
 apt-get update
 
 #######################################
@@ -33,13 +27,9 @@ apt-get update
 # Setup tools
 apt-get install -y git
 apt-get install -y sublime-text-installer
-apt-get install -y default-jdk  #needed for WebStorm
 apt-get install -y meld #visual diff/merge tool
 apt-get install -y jq #json query command line tool
 apt-get install -y vim
-
-# Setup python
-apt-get install -y python python-pip
 
 # Install node
 curl --silent --location https://deb.nodesource.com/setup_4.x | bash -
@@ -50,10 +40,6 @@ curl -sSL https://get.docker.com/ | sh
 
 # allow using docker as a non-root user
 usermod -aG docker vagrant
-
-# Add PEP8, Virtualenv
-pip install pep8
-pip install virtualenv
 
 # Setup Sublime Package Managers
 chown -R vagrant:vagrant /home/vagrant/.config/
@@ -72,25 +58,10 @@ git clone https://github.com/titoBouzout/SideBarEnhancements.git "$SUBLIME_PACKA
 git clone https://github.com/SublimeLinter/SublimeLinter3.git "$SUBLIME_PACKAGE_DIR"SublimeLinter
 git clone https://github.com/wbond/sublime_terminal.git "$SUBLIME_PACKAGE_DIR"sublime_terminal
 git clone https://github.com/jisaacks/GitGutter.git "$SUBLIME_PACKAGE_DIR"GitGutter
-git clone https://github.com/SublimeLinter/SublimeLinter-pep8.git "$SUBLIME_PACKAGE_DIR"SublimeLinter-pep8
 
 chown -R vagrant:vagrant "$SUBLIME_INSTALL_PACKAGE_DIR"
 chown -R vagrant:vagrant "$SUBLIME_PACKAGE_DIR"
 chown -R vagrant:vagrant "$SUBLIME_DIR"
-
-# Install WebStorm
-# Don't download if the package already exists locally on the host
-if [ ! -f /vagrant/WebStorm-$WEBSTORM_VERSION.tar.gz ]; then
-    echo "WebStorm not found. Downloading..."
-    wget -nv -P /vagrant/ http://download.jetbrains.com/webstorm/WebStorm-$WEBSTORM_VERSION.tar.gz
-
-fi
-
-tar -xvf /vagrant/WebStorm-$WEBSTORM_VERSION.tar.gz
-chown -R vagrant:vagrant WebStorm-$WEBSTORM_VERSION/
-
-# Install Chrome
-apt-get install -y google-chrome-stable
 
 # Setup meld as the default merge and diff tool
 su - vagrant -c "git config --global merge.tool meld"
